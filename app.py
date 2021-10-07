cfab1acdd8f2c59837eacbfb2b89523b3593862b
CyberWolf37 <borel.loic.37@gmail.com>
2021-10-07 22:42:23 +0200

    Add Flask import

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from database import Crypto, Database
+from flask import Flask, render_template
 from apicoin import ApiCoinMarkt as api
 from dotenv import load_dotenv
 
 
-if __name__ == "__main__":
-    load_dotenv("./app.env")
-    db = Database()
-    a = api(db)
-    a.get_quote()
+#if __name__ == "__main__":
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template('accueil.html')
+        
+    #load_dotenv("./app.env")
+    #db = Database()
+    #a = api(db)
+    #a.get_quote()
     #a.get_plot_by_name('ETH')
